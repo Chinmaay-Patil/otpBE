60faeb640abfaa468c1e7ae3790aadcf90fc74c3
Chinmaay-Patil <102859886+Chinmaay-Patil@users.noreply.github.com>
2022-11-16 09:31:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,18 +23,16 @@ app.use(cors())
 
 app.post("/send", async function (req, res) {
 
-  console.log(req.body.userdata)
+ 
   let msg = req.body.userdata.message + ' Your OTP is ' + Math.floor(100000 + Math.random() * 900000);
   const client = new twilio(process.env.TWILIO_SID,process.env.TWILIO_AUTH)
   return client.messages
   .create({body:msg,from: '+15135866760',to: req.body.userdata.phone})
-//   .then(message=> console.log(message,"message sent")
-//   ,res.send(userdata))
 .then ((message)=>{
     console.log(message,"msg sent")
     let mm= req.body.userdata
     mm.message =msg
-    console.log(mm)
+   
     res.send(mm)
 })
   .catch (err=> console.log(err,"message not sent"))
